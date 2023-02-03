0d6c5b71e0a02e5aabb86ccc400d0cd9614c6ffe
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-02-03 11:14:17 +0530

    chore: add dependencies

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// sinple express server
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
