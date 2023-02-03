a31dd5b866b524dbaa61d414d02408e43de691dc
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-02-03 14:30:24 +0530

    chore: add validators

--- src/middleware/company.validator.js
+++ src/middleware/company.validator.js
@@ -1,26 +1,52 @@
+const Joi = require('joi');
+
 const HttpErrors = require('../errors/httperror');
 
 const saveDataSchema = Joi.object({
-    urlLink: Joi.string().min(3).max(30).required(),
-  });
+  urlLink: Joi.string().min(3).max(30).required(),
+});
+
+const updateDataSchema = Joi.object({
+	companyName: Joi.string().min(3).max(30),
+	companyCEO: Joi.string().min(3).max(30)
+});
 
 const saveDataValidator = (req, res, next) => {
-    try {
-      const { error } = saveDataSchema.validate(req.body);
-      if (error) {
-        throw new HttpErrors(error.details[0].message, 400);
-      }
-      else {
-        next();
-      }
+  try {
+    const { error } = saveDataSchema.validate(req.body);
+    if (error) {
+      throw new HttpErrors(error.message, 400);
     }
-    catch (err) {
-      if (err instanceof HttpErrors) {
-        res.status(err.code).json({ 'message': err.message });
-      }
-      else
-        res.status(500).json({ 'message': 'Internal server error.' });
+    else {
+      next();
     }
+  }
+  catch (err) {
+    if (err instanceof HttpErrors) {
+      res.status(err.code).json({ 'message': err.message });
+    }
+    else
+      res.status(500).json({ 'message': 'Internal server error.' });
+  }
 };
 
-module.exports = { saveDataValidator };
+const updateDataValidator = (req, res, next) => {
+  try {
+    const { error } = updateDataSchema.validate(req.body);
+    if (error) {
+      throw new HttpErrors(error.message, 400);
+    }
+    else {
+      next();
+    }
+  }
+  catch (err) {
+    if (err instanceof HttpErrors) {
+      res.status(err.code).json({ 'message': err.message });
+    }
+    else
+      res.status(500).json({ 'message': 'Internal server error.' });
+  }
+};
+
+module.exports = { saveDataValidator, updateDataValidator };
--- src/routers/company.router.js
+++ src/routers/company.router.js
@@ -1,9 +1,10 @@
 const companyRouter = require('express').Router();
 
 const { saveData, fetchCompanyDataBySector, updateCompanyData } = require('../controllers/company.controller');
+const { saveDataValidator, updateDataValidator } = require('../middleware/company.validator');
 
-companyRouter.post('/api/save', saveData);
+companyRouter.post('/api/save', saveDataValidator, saveData);
 companyRouter.get('/api/companies', fetchCompanyDataBySector);
-companyRouter.put('/api/company/:companyId', updateCompanyData);
+companyRouter.put('/api/company/:companyId', updateDataValidator, updateCompanyData);
 
 module.exports = companyRouter;
